03e512ea5b5480c36d6754c6ef471156d61c56ea
Tommaso Panozzo <8103786+tom139@users.noreply.github.com>
2024-09-11 01:18:18 +0200

    feat: add fake teacher

--- app/lib/prof.ts
+++ app/lib/prof.ts
@@ -13,4 +13,16 @@ export const professors: Professor[] = [
       { klass: "3 BM", subject: "LETT" },
     ],
   },
+  {
+    name: "Rossi",
+    subjects: [
+      { klass: "2AS", subject: "ING" },
+      { klass: "2AS", subject: "TIC" },
+      { klass: "2BM", subject: "LETT" },
+      { klass: "3 ART", subject: "LETT" },
+      { klass: "1 ARTy", subject: "TDP" },
+      { klass: "4 BM", subject: "ING" },
+      { klass: "2 BM", subject: "ING" },
+    ],
+  },
 ];
